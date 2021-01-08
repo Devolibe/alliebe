09d3ce2273b5e465315e50a25630bdcbd3bcd35b
박주성 <jusung@bagjuseong-ui-MacBookPro.local>
2021-01-08 22:11:37 +0900

    login ac added

---
+++ app/src/main/java/com/alliebe/mastersejin/Login_Activity.kt
@@ -0,0 +1,11 @@
+package com.alliebe.mastersejin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Login_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_)
+    }
+}
