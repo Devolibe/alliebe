d79ef049ad8ceb0f1b8d8e4ecfd5d6859f2d19e0
Sejin <sejigner@gmail.com>
2021-02-18 18:01:30 +0900

    Close #9 - Implement bottom navigation bar in mainactivity
    The Bottom navigation bar consists of 4 fragments: Home, Lookaround, Search and Mypage.

---
+++ app/src/main/java/com/alliebe/mastersejin/HomeFragment.kt
@@ -0,0 +1,16 @@
+package com.alliebe.mastersejin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil.setContentView
+import androidx.fragment.app.Fragment
+
+class HomeFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        var view = LayoutInflater.from(activity).inflate(R.layout.fragment_home,container,false)
+        return view
+    }
+}
--- app/src/main/java/com/alliebe/mastersejin/ViewPagerAdapter.kt
+++ app/src/main/java/com/alliebe/mastersejin/ViewPagerAdapter.kt
@@ -4,17 +4,20 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
+import com.alliebe.mastersejin.HomeFragment
+import com.alliebe.mastersejin.MyPageFragment
 
 class ViewPagerAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm){
     override fun getItem(position: Int): Fragment {
         return when(position){
-            0 -> RecommendFragment()
+            0 -> HomeFragment()
             1 -> LookaroundFragment()
-            else -> QuestionFragment()
+            2 -> SearchFragment()
+            else -> MyPageFragment()
         }
     }
 
-    override fun getCount() = 3
+    override fun getCount() = 4
 
     override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
         super.destroyItem(container, position, `object`)
