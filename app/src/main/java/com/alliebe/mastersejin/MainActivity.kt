dbc8efb90c51343a41e9e68c040b0fb9c95df539
Sejin <sejigner@gmail.com>
2021-02-19 15:17:40 +0900

    Close #10 Create fundamental UIs in MainActivity
    In this branch, Toolbar, hamburger- and notification button in the toolbar, and floating action button(fab) are placed in mainactivity.

--- app/src/main/java/com/alliebe/mastersejin/MainActivity.kt
+++ app/src/main/java/com/alliebe/mastersejin/MainActivity.kt
@@ -2,11 +2,14 @@ package com.alliebe.mastersejin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.fragment.app.Fragment
 import com.alliebe.mastersejin.com.alliebe.mastersejin.LookaroundFragment
 import com.alliebe.mastersejin.com.alliebe.mastersejin.SearchFragment
 import com.alliebe.mastersejin.com.alliebe.mastersejin.ViewPagerAdapter
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
@@ -30,7 +33,22 @@ class MainActivity : AppCompatActivity() {
         myRef.child("message").setValue("Hello, World!")
         myRef.child("sejin").setValue("Hello, World!")
 
+        val fab = findViewById<FloatingActionButton>(R.id.edit_fab)
         initNavigationBar()
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        supportActionBar!!.setHomeAsUpIndicator(R.drawable.ic_hamburger)
+
+        /* 툴바 이미지 변경
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_logo)
+         */
+    }
+
+    // main_menu 메뉴 레이아웃을 toolbar 메뉴 버튼으로 설정
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
     }
 
     private fun initNavigationBar() {
